029f2eadfb343c7e5f319c081873ee181f01bf40
DevBaka <devbaka@gmx.net>
2019-09-26 19:57:24 +0200

    Create YoutubeMp3Converter.py

---
+++ YoutubeMp3Converter.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=0-c2CrQ4vJw&list=PLy_hoHKuZKvTrbgfOVVS_WCqW28W4dw3d&index=91'])
+
