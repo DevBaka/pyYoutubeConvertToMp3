94d2ca4195fa9143e2b29a4d593e702d8906944c
DevBaka <devbaka@protonmail.com>
2019-09-27 15:31:59 +0200

    some little changes: -add input in a while loop to ask links

--- YoutubeMp3Converter.py
+++ YoutubeMp3Converter.py
@@ -1,14 +1,23 @@
 from __future__ import unicode_literals
 import youtube_dl
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=0-c2CrQ4vJw&list=PLy_hoHKuZKvTrbgfOVVS_WCqW28W4dw3d&index=91'])
+def dlLink(link):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        #ydl.download(['https://www.youtube.com/watch?v=0-c2CrQ4vJw&list=PLy_hoHKuZKvTrbgfOVVS_WCqW28W4dw3d&index=91'])
+        ydl.download([link])
 
+
+
+link = ""
+while link != "exit":
+    link = raw_input("insert link or type 'exit' to exit the programm")
+    if link != "exit":
+        dlLink(link)
